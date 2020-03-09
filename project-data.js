af20b99926c4cde27ed266af5f1504e4ba4277d8
Megan Katherine O'Brien <megankob@gmail.com>
2020-03-09 15:46:18 -0400

    Setting up project data

---
+++ project-data.js
@@ -0,0 +1,20 @@
+//data for projects to be grabbed with graphQL
+//TODO: get and add images to use
+
+module.exports = {
+    projects: [
+        {
+            id: `0`,
+            project_name: `Random Quote Generator`,
+            description:   `A random quote and background color will be generated automatically or by clicking on the Another Quote button.`,
+            technologies: [
+                `JavaScript`,
+                `HTML`,
+                `CSS`
+            ],
+            live_link: `https://mk0b.github.io/Random-Quote-Generator-Project-1/`,
+            github_link: `https://github.com/mk0b/Random-Quote-Generator-Project-1`,
+            image_urls: []
+        }
+    ]
+}
