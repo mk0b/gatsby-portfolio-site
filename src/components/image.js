582b4c4d6d38cd52df7d773db65febc14c2aea07
Megan Katherine O'Brien <megankob@gmail.com>
2020-03-15 16:38:28 -0400

    Switched how i'm doing images

--- src/components/image.js
+++ src/components/image.js
@@ -8,6 +8,9 @@ import Img from 'gatsby-image';
  * component, rather than having to pass the image data down from pages. 
  * */
 
+ //FIXME: I cannot get Img to work. Works perfectly on my machine but when I deploy it the images wont render properly. 
+//There are others having these issues but I found no solution after two days. Will come back to it later and use regular image now.
+
 const Image = (props) => (<Img fixed={props.imageData.src.childImageSharp.fixed} alt={props.imageData.name} />);
 
 
--- src/components/project.js
+++ src/components/project.js
@@ -1,17 +1,19 @@
 import React, { Fragment } from 'react';
 
-import Image from './Image';
+//import Image from './Image';
 
-//use gatsby image to improve the images I'm using. 
 //for each project in the projects array in projects.js this component will be called
 //and will generate the indv project cards.
 
 const Project = (props) => {
 
+    console.log('Image Data: ', props.imageData);
+    console.log('src', props.imageData.src.childImageSharp.fixed.src);
+
     return(
         <li className="container">
             <h3 style={{ textAlign: 'center', color: `rgb(52,71,86)` }}>{props.name}</h3>
-                <Image imageData={props.imageData} />            
+            <img src={props.imageData.src.childImageSharp.fixed.src} alt={props.imageData.name} />
             <div className="overlay">
                 {/*checking live link and if it's an empty string only display github link*/}
                 {props.live_link ? (
