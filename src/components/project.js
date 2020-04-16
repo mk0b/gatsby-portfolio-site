e67f733faf050f59a0d24460deb141a027384b69
Megan Katherine O'Brien <megankob@gmail.com>
2020-04-16 13:18:41 -0400

    Updated tech color

--- src/components/project.js
+++ src/components/project.js
@@ -9,14 +9,15 @@ const Project = (props) => {
     
     //for now I removed sqlite from the tech lists to make things fit better
     //TODO: Find a better solution. I'm thinking of putting every tech into it's own span and being able to style them separately to create a better visual.
-    const technologies = props.technologies.join('  ');
+    //TODO: Made the tech items a bit smaller this may help too without changing everything
+    const technologies = props.technologies.join(' ');
 
     return(
         <li className="container">
             <h3 style={{ textAlign: 'center', color: `rgb(52,71,86)` }}>{props.name}</h3>
             <Image imageData={props.imageData} />
             <div className="technologies">
-                <h4>{technologies}</h4>
+                <h5>{technologies}</h5>
             </div>
             <div className="overlay">
                 {/*checking live link and if it's an empty string only display github link*/}
